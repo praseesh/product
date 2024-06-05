30ca6f8ff8d417e564bd7610df60c5ec90dce0bc
Praseesh <150511158+praseesh@users.noreply.github.com>
2024-06-05 14:32:13 +0530

    Update urls.py

--- product/urls.py
+++ product/urls.py
@@ -7,9 +7,6 @@ Examples:
 Function views
     1. Add an import:  from my_app import views
     2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 
 """
 from django.contrib import admin
