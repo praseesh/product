5807cffce68d33c1680bfd8953e8caf1552562f7
Praseesh <150511158+praseesh@users.noreply.github.com>
2024-08-16 22:25:58 +0530

    Update urls.py

--- product1/urls.py
+++ product1/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path ('',views.home, name = "home"),
     path('add/', views.add, name='add'),
     path('display/', views.display, name='display'),
-    path('update', views.update, name='update'),
+    path('update/', views.update, name='update'),
     path('delete', views.delete, name='delete'),
 
     path('accounts/login/', views.loginview, name = 'login'),
