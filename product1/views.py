cc62ea13fed5eb7cd5934d9966c12ca5c6f21a5a
Praseesh <150511158+praseesh@users.noreply.github.com>
2024-09-14 20:30:25 +0530

    Update views.py

--- product1/views.py
+++ product1/views.py
@@ -5,7 +5,6 @@ from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 
-
 @login_required
 def home(request):
     return render(request, "addlist.html")
