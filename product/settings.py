1e93b82ff0d7b44eaf4b2e0a6c9a4f9fd07f3e48
Praseesh <150511158+praseesh@users.noreply.github.com>
2025-01-09 21:23:39 +0530

    Update settings.py

--- product/settings.py
+++ product/settings.py
@@ -1,14 +1,10 @@
 from pathlib import Path
 import os
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-$2@j@vwh^c7jfxg!03frnrvfthkv#x^@olv0x5ahu(zp9*frnf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
